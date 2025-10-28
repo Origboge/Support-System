b3de1ef88bfc14169c02c32d38cb79eda7dbac0a
Bambam <akinbams11@gmail.com>
2025-10-28 18:59:09 +0100

    VT

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/support-system/', // <-- The path is now correctly set to your repo name!
+    base: '/Support-System/', // <-- The path is now correctly set to your repo name!
 })
